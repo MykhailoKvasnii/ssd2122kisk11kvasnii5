6c9ccda93fc566240f43d19f4041ffaab6f1cbde
MykhailoKvasnii <mykhailo.kvasnii.mkisk.2021@lpnu.ua>
2022-06-04 19:16:47 +0300

    "Added writing and reading from the file"

--- SSD_Lab1/FileReader.cs
+++ SSD_Lab1/FileReader.cs
@@ -1,13 +1,21 @@
 using System;
 using System.IO;
-//INI
 using IniParser;
+using IniParser.Model;
+using System.Xml.Linq;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
 
 namespace SoftwareSystemDesignApp
 {
     // Class for reading sequence from files with .ini, .xml and .json extensions
     public static class FileReader
     {
+        // Variables for files internal data path
+        private const string FILES_SECTION_NAME = "DataModel";
+        private const string FILES_TAGS_NAME = "Sequence";
+
         /// <summary>
         /// Choose implementation of file read method by it extension
         /// </summary>
@@ -16,9 +24,9 @@ namespace SoftwareSystemDesignApp
         static public string ReadDataFromFile(string filePath)
         {
             string extension = Path.GetExtension(filePath);
-            string data = null;
+            string data = "";
             try
-            {           
+            {
                 // Choose file reader implementation by file extension
                 switch (extension)
                 {
@@ -26,14 +34,21 @@ namespace SoftwareSystemDesignApp
                         data = ReadFromINI(filePath);
                         break;
                     case ".json":
-                        data = ReadFromJSONOrXML(filePath);
+                        data = ReadFromJSON(filePath);
                         break;
                     case ".xml":
-                        data = ReadFromJSONOrXML(filePath);
+                        data = ReadFromXML(filePath);
                         break;
                     default:
                         Console.Clear();
-                        Console.WriteLine("Unknown file extencion. Please reenter file path or enter '-sf' command again to exit from this menu.");
+                        if (data == null)
+                        {
+                            Console.WriteLine("Section with sequence wasn't founded in this file.");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Unknown file extencion. Please reenter file path or enter '-sf' command again to exit from this menu.");
+                        }
                         break;
                 }
                 return data;
@@ -54,17 +69,59 @@ namespace SoftwareSystemDesignApp
         static string ReadFromINI(string pathINI)
         {
             var parser = new FileIniDataParser();
-            return parser.ReadFile(pathINI).ToString();
+            IniData data = parser.ReadFile(pathINI);
+            // Iterate through all the sections
+            foreach (SectionData section in data.Sections)
+            {
+                if (section.SectionName == FILES_SECTION_NAME)
+                {
+                    foreach (KeyData key in section.Keys)
+                    {
+                        if (key.KeyName == FILES_TAGS_NAME)
+                        {
+                            return key.Value.Replace("\"", "");
+                        }
+                    }
+                }
+            }
+            return null; // return error if not find data at file format adress
         }
 
         /// <summary>
-        /// Read sequence from JSON, XML file
+        /// Read sequence from JSON file
         /// </summary>
         /// <param name="pathJSON">Path to JSON file</param>
         /// <returns>Sequence from JSON file</returns>
-        static string ReadFromJSONOrXML(string pathJSON)
+        static string ReadFromJSON(string pathJSON)
         {
-            return File.ReadAllText(pathJSON);
+            // Read JSON directly from a file
+            using (StreamReader file = File.OpenText(pathJSON))
+            using (JsonTextReader reader = new JsonTextReader(file))
+            {
+                JObject json = (JObject)JToken.ReadFrom(reader);
+                return json[FILES_TAGS_NAME].Value<string>();
+            }
+            return null; // return error if not find data at file by format adress
+        }
+
+        /// <summary>
+        /// Read sequence from XML file
+        /// </summary>
+        /// <param name="pathXML">Path to XML file</param>
+        /// <returns>Sequence from XML file</returns>
+        static string ReadFromXML(string pathXML)
+        {
+            XElement xelement = XElement.Load(pathXML);
+            IEnumerable<XElement> sequenceData = xelement.Elements();
+            // Iterate through all the sections
+            foreach (var sequence in sequenceData)
+            {
+                if (sequence.Name.LocalName == FILES_TAGS_NAME)
+                {
+                    return sequence.FirstNode.Parent.Value;
+                }
+            }
+            return null; // return error if not find data at file format adress
         }
     }
 }
--- SSD_Lab1/Program.cs
+++ SSD_Lab1/Program.cs
@@ -1,15 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SoftwareSystemDesignApp
 {
+    // Main class (Viewer)
     class Program
     {
-        private const string VERSION_NUMBER = "1";
+        // Menu options
         private static readonly Dictionary<string, string> OPTIONS = new Dictionary<string, string>()
         {
             {"-sf", "set sequence from file"},
@@ -19,108 +16,163 @@ namespace SoftwareSystemDesignApp
             {"-v", "get software version"},
             {"-lv", "exit from program"}
         };
-        private static bool isSequenseWasRecieved;
-        private static bool isNumberWasRecieved;
+        private static bool isSequenseWasRecieved; // flag for calling sequence execution
+        private static bool isNumberWasRecieved; // flag for calling sequence execution
+        private static bool isSequenseShouldBeStoredInFile; // flag for storing current sequnce if file
 
         static void Main(string[] args)
         {
+            // Infinite loop - console menu
             while (true)
             {
                 // Display command options
-                Console.WriteLine("Please enter one of following command, enter '-h' for help:");
-                foreach(var option in OPTIONS)
-                {
-                    Console.WriteLine($"{option.Key, 3}, {option.Value}");
-                }
+                PrintMenu();
                 string userOption = Console.ReadLine();
                 Console.Clear();
-                              
+
                 // Menu implementation
                 switch (userOption)
-                {                  
-                    case "-sf":                    
-                        Console.WriteLine("Enter location to file with SF, or enter again '-sf' to exit from this menu:");
+                {
+                    // Read sequnce from file .ini, .xml, .json extention case
+                    case "-sf":
+                        Console.WriteLine("Enter location to file with SF, or enter again '-sf' to exit from this menu option:");
                         while (true)
                         {
                             string filePath = Console.ReadLine();
-                            if(filePath == "-sf")
+                            if (filePath == "-sf")
                             {
                                 Console.Clear();
                                 break;
                             }
-                            // C:\torrent\TestData\TestFile.ini
-                            string dataFromFile = FileReader.ReadDataFromFile(filePath);
-                            if(dataFromFile != null)
-                            {   
-                                Calculation.GetSequnceByFile(dataFromFile);
-                                Console.WriteLine(Calculation.Sequence); // Visualize sequence to user
-                                isSequenseWasRecieved = true;
+                            if (isSequenseShouldBeStoredInFile && !string.IsNullOrEmpty(Calculation.GetSequence()))
+                            {
+                                FileWriter.WriteSequenceToFile(filePath);
                                 break;
-                            }                       
-                        }                  
-                        break;
+                            }
+                            else
+                            {
+                                // C:\torrent\TestData\TestFile.ini
+                                string dataFromFile = FileReader.ReadDataFromFile(filePath);
+                                if (string.IsNullOrEmpty(dataFromFile))
+                                {
+                                    Console.WriteLine(dataFromFile); // Visualize sequence to user
+                                }
 
-                    case "-s":   
-                        Console.WriteLine("Enter sequence:");   
-                        Calculation.GetSequnceByManualTyping(Console.ReadLine());
-                        isSequenseWasRecieved = true;
+                                if (Calculation.IsSequnceCorrect(dataFromFile))
+                                {
+                                    Calculation.SetSequnce(dataFromFile);
+                                    isSequenseWasRecieved = true;
+                                    break;
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Entered sequence contains validation error. Please enter new file path or '-sf' to to exit from this menu option.");
+                                }
+                            }
+                        }
                         break;
-
-                    case "-n":
-                        Console.WriteLine("Enter number of sequnce elements");
-                        
-                        bool isresultOfParsingCorrect, isNumberPositive;
-                        int numberOfSequnce;
-                        // Verify that entered number is correct (uint)
+                    // Read sequence from console case
+                    case "-s":
+                        Console.WriteLine("Enter sequnce, or enter again '-s' to exit from this menu option:");
                         while (true)
                         {
-                            isresultOfParsingCorrect = int.TryParse(Console.ReadLine(), out numberOfSequnce); // Verify that number is integer
-                            isNumberPositive = numberOfSequnce > 0 ? true : false; // Verify that number at least '1'
-                            if(isresultOfParsingCorrect && isNumberPositive)
+                            string sequenceValue = Console.ReadLine();
+                            if (sequenceValue == "-s")
                             {
+                                Console.Clear();
+                                break;
+                            }
+                            // Verify that entered sequence is correct
+                            if (Calculation.IsSequnceCorrect(sequenceValue))
+                            {
+                                Calculation.SetSequnce(sequenceValue);
+                                isSequenseWasRecieved = true;
+                                isSequenseShouldBeStoredInFile = true;
+                                Console.Clear();
+                                break;
+                            }
+                            else
+                            {
+                                Console.Clear();
+                                Console.WriteLine("Entered sequence contains validation error. Please enter new sequnce.");
+                            }
+                        }
+                        break;
+                    // Read number of elements from console case
+                    case "-n":
+                        Console.WriteLine("Enter number of sequnce elements, or enter again '-n' to exit from this menu option:");
+                        while (true)
+                        {
+                            string numberOfSequence = Console.ReadLine();
+                            if (numberOfSequence == "-n")
+                            {
+                                Console.Clear();
+                                break;
+                            }
+                            // Verify that entered number is correct (uint)
+                            if (Calculation.IsNumberCorrect(numberOfSequence))
+                            {
+                                Calculation.SetNumberOfSequnceElements(int.Parse(numberOfSequence));
+                                isNumberWasRecieved = true;
+                                Console.Clear();
                                 break;
                             }
                             else
                             {
                                 Console.Clear();
                                 Console.WriteLine("Wrong format of number. Please enter only positive integer type (max - 2147483647).");
-                            }                          
+                            }
                         }
-                        Calculation.GetNumberOfSequnceElements(numberOfSequnce);
-                        isNumberWasRecieved = true;
                         break;
-
+                    // Call user manual case
                     case "-h":
                         Calculation.CallUserHelpInfo();
                         break;
-
+                    // Call version number case
                     case "-v":
                         Console.WriteLine(Calculation.CallVersionNumber());
                         break;
-
+                    // Exit from program case
                     case "-lv":
                         Calculation.ExitFromProgram();
                         break;
-
                     default:
                         break;
                 }
 
-                // Display info that .csv file was created
+                // Execute sequence calculation
                 if (isSequenseWasRecieved && isNumberWasRecieved)
                 {
-                    if (Calculation.IsSequnceCorrect(Calculation.Sequence))
-                    {
-                        Console.WriteLine("Answer: " + Calculation.CalculateSequnceElements(Calculation.Sequence, Calculation.NumberOfSequnce));
-                        isNumberWasRecieved = false;
-                        isSequenseWasRecieved = false;
-                    }
-                    else
-                    {
-                        Console.WriteLine("Entered sequence contains validation error. Please enter new sequnce.");
-                    }                  
+                    CalculateAndSaveResults();
+                    isNumberWasRecieved = false;
+                    isSequenseWasRecieved = false;
+                    isSequenseShouldBeStoredInFile = false;
                 }
             }
+        }
+
+        /// <summary>
+        /// Method-helper for printing menu options
+        /// </summary>
+        private static void PrintMenu()
+        {
+            Console.WriteLine("===========================================================");
+            Console.WriteLine("Please enter one of following command, enter '-h' for help:");
+            foreach (var option in OPTIONS)
+            {
+                Console.WriteLine($"{option.Key,3}, {option.Value}");
+            }
+            Console.WriteLine("===========================================================");
+        }
+
+        /// <summary>
+        /// Method-helper for calling calculating and saving results
+        /// </summary>
+        private static void CalculateAndSaveResults()
+        {
+            Calculation.CalculateSequnceElements();
+            Console.WriteLine(Calculation.GetSequnceResults());
+            Calculation.SendSequnceResultToWriter();
         }
     }
 }
